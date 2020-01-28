a68a4f6362f787c703301ba1a0c7a0ae51d35a43
narek <narek.hakobyan.07@gmail.com>
2020-01-28 21:18:57 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# top_sheet
+
+flutter Top sheet package
+
+## Getting Started
+
+### installation
+
+install latest version from [Pub](https://pub.dev/packages/top_sheet)
+
+### Usage
+
+```dart
+class TopSheetDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            child: Text("Click Me"),
+            onPressed: () {
+              TopSheet.show(
+                context: context,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.music_note),
+                      title: Text('Music'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.photo_album),
+                      title: Text('Photos'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.videocam),
+                      title: Text('Video'),
+                    ),
+                  ],
+                ),
+                direction: TopSheetDirection.TOP,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+```
+
+This project is a starting point for a Dart
+[package](https://flutter.dev/developing-packages/),
+a library module containing code that can be shared easily across
+multiple Flutter or Dart projects.
+
+For help getting started with Flutter, view our 
+[online documentation](https://flutter.dev/docs), which offers tutorials, 
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/top_sheet.dart
@@ -0,0 +1,163 @@
+library top_sheet;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+@immutable
+class TopSheet extends StatefulWidget {
+  final TopSheetDirection direction;
+  final Color backgroundColor;
+  final Widget child;
+
+  TopSheet({this.child, this.direction, this.backgroundColor});
+
+  @override
+  _TopSheetState createState() => _TopSheetState();
+
+  static show(
+      {@required BuildContext context,
+      @required Widget child,
+      direction = TopSheetDirection.BOTTOM,
+      backgroundColor = const Color(0xb3212121)}) {
+    Navigator.push(
+        context,
+        PageRouteBuilder(
+            pageBuilder: (BuildContext context, Animation<double> animation,
+                Animation<double> secondaryAnimation) {
+              return TopSheet(
+                child: Container(
+                  color: Colors.white,
+                  child: SafeArea(
+                    child: child,
+                  ),
+                ),
+                direction: direction,
+                backgroundColor: backgroundColor,
+              );
+            },
+            opaque: false));
+  }
+}
+
+class _TopSheetState extends State<TopSheet> with TickerProviderStateMixin {
+  Animation<double> _animation;
+  Animation<double> _opacityAnimation;
+  AnimationController _animationController;
+
+  final _childKey = GlobalKey();
+
+  double get _childHeight {
+    final RenderBox renderBox = _childKey.currentContext.findRenderObject();
+    return renderBox.size.height;
+  }
+
+  bool get _dismissUnderway =>
+      _animationController.status == AnimationStatus.reverse;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+
+    _animation = Tween<double>(begin: _isDirectionTop ? -1 : 1, end: 0)
+        .animate(_animationController);
+
+    _opacityAnimation =
+        Tween<double>(begin: 0, end: 0.7).animate(_animationController);
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.dismissed) Navigator.pop(context);
+    });
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _handleDragUpdate(DragUpdateDetails details) {
+    if (_dismissUnderway) return;
+
+    var change = details.primaryDelta / (_childHeight ?? details.primaryDelta);
+    if (_isDirectionTop)
+      _animationController.value += change;
+    else
+      _animationController.value -= change;
+  }
+
+  void _handleDragEnd(DragEndDetails details) {
+    if (_dismissUnderway) return;
+
+    if (details.velocity.pixelsPerSecond.dy > 0 && _isDirectionTop) return;
+    if (details.velocity.pixelsPerSecond.dy < 0 && !_isDirectionTop) return;
+
+    if (details.velocity.pixelsPerSecond.dy > 700) {
+      final double flingVelocity =
+          -details.velocity.pixelsPerSecond.dy / _childHeight;
+      if (_animationController.value > 0.0)
+        _animationController.fling(velocity: flingVelocity);
+    } else if (_animationController.value < 0.5) {
+      if (_animationController.value > 0.0)
+        _animationController.fling(velocity: -1.0);
+    } else
+      _animationController.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
+    return WillPopScope(
+      onWillPop: onBackPressed,
+      child: GestureDetector(
+        onVerticalDragUpdate: _handleDragUpdate,
+        onVerticalDragEnd: _handleDragEnd,
+        child: AnimatedBuilder(
+          animation: _opacityAnimation,
+          builder: (context, child) {
+            return Scaffold(
+              backgroundColor:
+                  widget.backgroundColor.withOpacity(_opacityAnimation.value),
+              body: Column(
+                key: _childKey,
+                children: <Widget>[
+                  _isDirectionTop ? Container() : Spacer(),
+                  AnimatedBuilder(
+                      animation: _animation,
+                      builder: (context, _) {
+                        return Transform(
+                          transform: Matrix4.translationValues(
+                              0.0, width * _animation.value, 0.0),
+                          child: Container(
+                            width: width,
+                            child: widget.child,
+                          ),
+                        );
+                      }),
+                ],
+              ),
+            );
+          },
+        ),
+        excludeFromSemantics: true,
+      ),
+    );
+  }
+
+  bool get _isDirectionTop {
+    return widget.direction == TopSheetDirection.TOP;
+  }
+
+  Future<bool> onBackPressed() {
+    _animationController.reverse();
+    return Future<bool>.value(false);
+  }
+}
+
+enum TopSheetDirection { TOP, BOTTOM }
